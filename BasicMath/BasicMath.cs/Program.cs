0730a97a8505080c54904f22e0473d49a60bcb7a
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-14 08:17:03 -0700

    Assignment

---
+++ BasicMath/BasicMath.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicMath.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 14 + 25;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.Read();
+
+            int difference = 18 - 5;
+            Console.Write(difference);
+            Console.Read();
+        }
+    }
+}
