41f5406be1dd5580bd07e0a74ee1e8a46dacdcd6
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-16 01:21:56 -0700

    Branching Assignment

---
+++ Branching/Branching.cs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Branching.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.Write("Please enter the package weight: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            Console.Write("Please enter the package width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the package height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the package length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            double dimensionsTotal = width + height + length;
+
+            if (dimensionsTotal > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                return; 
+            }
+
+            double dimensionsMult = width * height * length;
+
+            double quote = (dimensionsMult * weight) / 100;
+
+            Console.WriteLine($"Your estimated total for shipping this package is: ${quote:F2}");
+
+            Console.ReadLine();
+        }
+    }
+}
