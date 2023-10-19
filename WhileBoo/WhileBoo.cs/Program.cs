172c836cbe947427aadceca606bc3e1e3fda1118
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-19 07:22:00 -0700

    Boolean Assignment

---
+++ WhileBoo/WhileBoo.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WhileBoo.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the number of apples: ");
+            int apples = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of oranges: ");
+            int oranges = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Using a do-while statement:");
+            do
+            {
+                Console.WriteLine("I have " + oranges + " oranges.");
+                oranges++;
+            } while (oranges <= apples);
+
+            Console.WriteLine("Using a while statement:");
+            while (apples > 0)
+            {
+                Console.WriteLine("I have " + apples + " apples.");
+                apples--;
+            }
+        }
+    }
+}
