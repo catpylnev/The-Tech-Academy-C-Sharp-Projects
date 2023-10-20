f93cfa32a2a4c3a330b86bc234cac5b24769ef7c
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-20 09:47:51 -0700

    ExceptionHandle

---
+++ ExceptionHandle/ExceptionHandle.cs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExceptionHandle.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = { 10, 20, 30, 40, 50 };
+                Console.Write("Enter a number to divede by: ");
+                int divide = int.Parse(Console.ReadLine());
+
+                foreach (int num in numbers)
+                {
+                    int result = num / divide;
+                    Console.WriteLine($"{num} divided by {divide} is {result}");
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"Error: {e.Message} You cannot divide by zero.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Error: {e.Message} Please enter a valid number");
+            }
+
+            Console.WriteLine("The program has finished.");
+        }
+    }
+}
