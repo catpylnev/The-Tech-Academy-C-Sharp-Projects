9cc5c98f2e18912c5988e5bb611644ce4efd19b9
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-21 12:44:40 -0700

    Calling Methods Assignment

---
+++ CallingMethods/CallingMethods.cs/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods.cs
+{
+    class MathOperations
+    {
+        public int Add(int number)
+        {
+            return number + 14;
+        }
+
+        public int Subtract(int number)
+        {
+            return number - 32;
+        }
+
+        public int Multiply(int number)
+        {
+            return number * 3;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CallingMethods.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            if (int.TryParse(Console.ReadLine(), out int userInput))
+            {
+                MathOperations mathOperations = new MathOperations();
+
+                int resultAdd = mathOperations.Add(userInput);
+                int resultSubtract = mathOperations.Subtract(userInput);
+                int resultMultiply = mathOperations.Multiply(userInput);
+
+                Console.WriteLine($"Result of addition: {resultAdd}");
+                Console.WriteLine($"Result of Subtraction: {resultSubtract}");
+                Console.WriteLine($"Result of Multiplication: {resultMultiply}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please Enter a valid integer.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
