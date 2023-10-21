519142e119347ae2a8ac8506bd2e2c2da7baab24
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-21 13:18:37 -0700

    Method Overload

---
+++ MathMethod/MathMethod.cs/MethodCalc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class MathCalculator
+{
+    public int PerformMathOperation(int number)
+    {
+        return number + 47;
+    }
+
+    public int PerformMathOperation(decimal number)
+    {
+        return (int)(number * 366);
+    }
+
+    public int PerformMathOperation(string input)
+    {
+        if (int.TryParse(input, out int parsedNumber))
+        {
+            return parsedNumber - 2;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+            return 0;
+        }
+    }
+}
---
+++ MathMethod/MathMethod.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathCalculator mathCalculator = new MathCalculator();
+
+        int resultInt = mathCalculator.PerformMathOperation(10);
+        Console.WriteLine($"Result of integer operation: {resultInt}");
+
+        int resultDecimal = mathCalculator.PerformMathOperation(5.5m);
+        Console.WriteLine($"Result of decimal operation: {resultDecimal}");
+
+        int resultString = mathCalculator.PerformMathOperation("7");
+        Console.WriteLine($"Result of string operation: {resultString}");
+
+        Console.ReadLine(); 
+    }
+}
