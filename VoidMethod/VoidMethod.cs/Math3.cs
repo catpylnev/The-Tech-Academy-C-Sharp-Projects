984d50172b5571fdaaa6918058439ba22b514eb5
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-22 05:05:12 -0700

    Void Method

---
+++ VoidMethod/VoidMethod.cs/Math3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethod.cs
+{
+    public class Math3
+    {
+        public void PerformMathOperationAndDisplay(int number1, int number2)
+        {
+            int result = number1 * 2;
+
+            Console.WriteLine($"Result of the main operation on the first integer: {result}");
+            Console.WriteLine($"Second integer: {number2}");
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VoidMethod.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math3 mathCalculator = new Math3();
+
+            int num1 = 5;
+            int num2 = 10;
+
+            mathCalculator.PerformMathOperationAndDisplay(num1, num2);
+            mathCalculator.PerformMathOperationAndDisplay(number1: 7, number2: 15);
+
+            Console.ReadLine();
+        }
+    }
+}
