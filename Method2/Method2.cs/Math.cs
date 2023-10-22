f9a0942e6ff5758effb979a7ba88f59e07a245d8
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-22 03:24:48 -0700

    Math Methods

---
+++ Method2/Method2.cs/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method2.cs
+{
+    public class Math
+    {
+        public int PerformMathOperation(int number1, int number2=0)
+        {
+            return number1 - number2;
+        }
+    }
+}
---
+++ Method2/Method2.cs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Method2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathCalculator = new Math();
+
+            Console.Write("Enter the first number: ");
+            if (int.TryParse(Console.ReadLine(), out int num1))
+            {
+                Console.Write("Enter the second number (or press enter to skip): ");
+                if (int.TryParse(Console.ReadLine(), out int num2))
+                {
+                    int result = mathCalculator.PerformMathOperation(num1, num2);
+                    Console.WriteLine($"Result of subtraction: {result}");
+                }
+                else
+                {
+                    int result = mathCalculator.PerformMathOperation(num1);
+                    Console.WriteLine($"Result of subtraction with one number: {result}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter valid integers.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
