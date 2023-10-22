170325fddc93e229a6928b37344e938d2defd383
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-22 05:52:39 -0700

    Overload Static Updat

---
+++ OverloadStatic/OverloadStatic.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OverloadStatic.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // VoidMath math = new VoidMath();
+
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int userNumber))
+            {
+                VoidMath.DivideByThree(userNumber);
+
+                int output;
+                VoidMath.DivideByThreeWithOutput(userNumber, out output);
+                Console.WriteLine($"Output with output parameter: {output}");
+
+                double doubleOutput;
+                VoidMath.DivideByThreeWithOutput(userNumber, out doubleOutput);
+                Console.WriteLine($"Double output: {doubleOutput}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OverloadStatic/OverloadStatic.cs/VoidMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadStatic.cs
+{
+    public class VoidMath
+    {
+        public static void DivideByThree(int number)
+        {
+            int result = number / 3;
+            Console.WriteLine($"Result of division by 3: {result}");
+        }
+
+        public static void DivideByThreeWithOutput(int number,out int result)
+        {
+            result = number / 3;
+        }
+
+        public static void DivideByThreeWithOutput(int number, out double result)
+        {
+            result = number / 3.0;
+        }
+    }
+}
