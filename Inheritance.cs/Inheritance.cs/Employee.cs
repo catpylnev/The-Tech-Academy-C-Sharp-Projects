2ef8a31b37eb46df679720fc8a7fffba0a1baded
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-22 07:10:09 -0700

    Inheritance Assignment

---
+++ Inheritance.cs/Inheritance.cs/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance.cs
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine($"Employee Name: {FirstName} {LastName}");
+        }
+    }
+}
---
+++ Inheritance.cs/Inheritance.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Inheritance.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee
+            {
+                FirstName = "Catherine",
+                LastName = "Pylnev",
+                Id = 12345
+            };
+
+            employee.SayName();
+
+            Console.ReadLine(); 
+        }
+    }
+}
