d9cb8286bb3ba3ca85d2523df06e3fa549e2b73f
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-25 10:29:48 -0700

    Abstract Assignment

---
+++ AbstractClass/AbstractClass.cs/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass.cs
+{
+    public class Employee : Person
+    {
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine($"Employee's name: {FirstName} {LastName}");
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbstractClass.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Sample", "Student");
+            {
+                employee.SayName();
+                Console.ReadLine();
+            };
+        }
+    }
+}
