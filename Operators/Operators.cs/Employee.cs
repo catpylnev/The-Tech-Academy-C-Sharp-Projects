d96ff47937d4716b0bdae2c994c309e07c789c97
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-26 08:53:10 -0700

    Operators Assignment

---
+++ Operators/Operators.cs/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators.cs
+{
+    class Employee
+    {
+        public int ID { get; set; }
+  
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee otherEmployee)
+            {
+                return this.ID == otherEmployee.ID;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+    }
+}
---
+++ Operators/Operators.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operators.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee { ID = 1 };
+            Employee employee2 = new Employee { ID = 2 }; //change IDs here
+
+            bool areEqual = employee1.Equals(employee2);
+
+            Console.WriteLine($"Are employee1 and employee2 equal? {areEqual}");
+        }
+    }
+}
