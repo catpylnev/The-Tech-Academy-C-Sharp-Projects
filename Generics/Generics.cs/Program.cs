4a4aa129d68d800e9f29921a5096d303e8a0b910
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-26 10:08:16 -0700

    Generic Parameters Assignment

---
+++ Generics/Generics.cs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employeeWithString = new Employee<string>();
+
+            employeeWithString.Things = new List<string>
+            {
+                "Thing 1",
+                "Thing 2",
+                "Thing 3"
+            };
+
+            Employee<int> employeeWithInt = new Employee<int>();
+
+            employeeWithInt.Things = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            Console.Write("Things of Employee with string type:");
+            foreach (string thing in employeeWithString.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("\n Things of Employee with int type:");
+            foreach(int thing in employeeWithInt.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
