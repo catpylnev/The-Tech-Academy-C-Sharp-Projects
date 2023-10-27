968c760eb40fb2fa69f2e5927b32ce87f69ee656
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-10-27 04:02:09 -0700

    Enum Assignment

---
+++ UsingEnums.cs/UsingEnums.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UsingEnums.cs
+{
+    class Program
+    {
+        public enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the current day of the week: ");
+                string userInput = Console.ReadLine();
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, ignoreCase: true);
+                Console.WriteLine("You entered: " + currentDay);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+}
