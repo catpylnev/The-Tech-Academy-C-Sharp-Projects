fd277f1c3581eaf553febe51fe6b349d8a278c48
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-11-01 08:54:00 -0700

    DateTime Assignment

---
+++ Datetime/Datetime.cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Datetime.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("Current Date and Time: " + currentTime);
+
+            Console.Write("Enter a number of hours: ");
+            if (int.TryParse(Console.ReadLine(), out int hours))
+            {
+                DateTime futureTime = currentTime.AddHours(hours);
+                Console.WriteLine($"Time in {hours} hours will be: {futureTime}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input. Please enter a valid number of hours.");
+            }
+        }
+    }
+}
